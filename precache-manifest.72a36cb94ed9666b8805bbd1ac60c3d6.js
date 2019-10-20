65793933a001862a35ed49dbfcf79e90e0386866
Hunter Hunnie Bastard <scrapcupcake@gmail.com>
2019-10-20 16:31:01 -0700

    Initial commit

---
+++ precache-manifest.72a36cb94ed9666b8805bbd1ac60c3d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dceb5841db6efb028d21ec35da9fefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dda2bb93bb5475d5f92",
+    "url": "/static/css/main.95a3c3e5.chunk.css"
+  },
+  {
+    "revision": "5bb00a9c43d3fe17ceec",
+    "url": "/static/js/2.d0a44802.chunk.js"
+  },
+  {
+    "revision": "2dda2bb93bb5475d5f92",
+    "url": "/static/js/main.5a5b61bd.chunk.js"
+  },
+  {
+    "revision": "99b22a354aad18e9f6cb",
+    "url": "/static/js/runtime-main.79422267.js"
+  }
+]);
