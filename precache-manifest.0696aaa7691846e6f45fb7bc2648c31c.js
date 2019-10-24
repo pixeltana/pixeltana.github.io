6c2c28227130257f79ac26f21494bff6277c8493
Elizabeth Tana <scrapcupcake@gmail.com>
2019-10-24 12:07:46 -0700

    Updates

---
+++ precache-manifest.0696aaa7691846e6f45fb7bc2648c31c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbdee478c3d4ab728faa06ea29896b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333baa2ce1dc7f21a2b1",
+    "url": "/static/css/main.c0f447a6.chunk.css"
+  },
+  {
+    "revision": "c3f2a70a5a6c0799b3b9",
+    "url": "/static/js/2.c42e7aa3.chunk.js"
+  },
+  {
+    "revision": "333baa2ce1dc7f21a2b1",
+    "url": "/static/js/main.6a1be230.chunk.js"
+  },
+  {
+    "revision": "99b22a354aad18e9f6cb",
+    "url": "/static/js/runtime-main.79422267.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f9b1334e96927cfc3f1f4143f5485c7.js"
+  "/precache-manifest.0696aaa7691846e6f45fb7bc2648c31c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
