073a620f4f03d2c86dce18aab14ba329c50e1fad
Elizabeth Tana <scrapcupcake@gmail.com>
2019-10-24 15:24:54 -0700

    New stuff

--- precache-manifest.cc32bf919e36471d671a9398102ecee6.js
+++ precache-manifest.cc32bf919e36471d671a9398102ecee6.js
@@ -1,33 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<< HEAD:precache-manifest.cc32bf919e36471d671a9398102ecee6.js
     "revision": "9b35c9bc9a0c860ed46acce94fc0d74a",
     "url": "/index.html"
   },
   {
     "revision": "1e9ffacc97edf456609e",
     "url": "/static/css/main.c0f447a6.chunk.css"
-=======
-    "revision": "ee771caf7f0fe90a927c877a7e1542d1",
-    "url": "/index.html"
-  },
-  {
-    "revision": "5f60800bc36b16cb39e4",
-    "url": "/static/css/main.853da4b3.chunk.css"
->>>>>>> 22899bd438de6c3217efb1520ea7521ea7134c89:precache-manifest.2f9dfb3254ab5362d01cffac6d634ef0.js
   },
   {
     "revision": "73800ad714e4095c6e54",
     "url": "/static/js/2.04b1497c.chunk.js"
   },
   {
-<<<<<<< HEAD:precache-manifest.cc32bf919e36471d671a9398102ecee6.js
     "revision": "1e9ffacc97edf456609e",
     "url": "/static/js/main.0735f219.chunk.js"
-=======
-    "revision": "5f60800bc36b16cb39e4",
-    "url": "/static/js/main.d1aec935.chunk.js"
->>>>>>> 22899bd438de6c3217efb1520ea7521ea7134c89:precache-manifest.2f9dfb3254ab5362d01cffac6d634ef0.js
   },
   {
     "revision": "99b22a354aad18e9f6cb",
